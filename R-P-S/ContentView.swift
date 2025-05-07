037481444e0d4d5c29b38e163b17f1387dc71f1f
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-05-07 23:00:53 +0300

    Version 0.0.1 Implements basic game UI.
    Adds a basic user interface for the Rock Paper Scissors game.
    Includes image assets for rock, paper, and scissors options,
    as well as display elements for game results.

--- R-P-S/ContentView.swift
+++ R-P-S/ContentView.swift
@@ -9,13 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Rock Paper Scissors")
+                        .foregroundStyle(Color(.white))
+                        .font(.largeTitle)
+                        .padding()
+                }
+                
+                VStack {
+                    Spacer()
+                    Text("Choose your weapon:")
+                        .foregroundStyle(Color(.white))
+                        .padding()
+                    Button{
+                        
+                    } label: {
+                        Image("generated-image(1)")
+                            .resizable()
+                            .frame(width: 125, height: 125)
+                    }
+                    
+                    
+                    Button{
+                        
+                    } label: {
+                        Image("generated-image(2)")
+                            .resizable()
+                            .frame(width: 115, height: 115)
+                    }
+                    
+                    Button{
+                        
+                    } label: {
+                        Image("generated-image(3)")
+                            .resizable()
+                            .frame(width: 130, height: 130)
+                    }
+                    
+                    Spacer()
+                    
+                    
+                        VStack {
+                            Text("Results:")
+                                .foregroundStyle(Color(.white))
+                                .font(.largeTitle)
+                                .padding()
+                            HStack {
+                                Text("You:")
+                                    .foregroundStyle(Color(.white))
+                                    .font(.title)
+                                    .padding()
+                                Text("Bot:")
+                                    .foregroundStyle(Color(.white))
+                                    .font(.title)
+                                    .padding()
+                            }
+                        }
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
