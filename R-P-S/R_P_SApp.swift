066210bd3358378cf8cdd61fdb6a9c24181945c8
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-05-07 22:25:00 +0300

    Initial Commit

---
+++ R-P-S/R_P_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_P_SApp.swift
+//  R-P-S
+//
+//  Created by Adrian Saca on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct R_P_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
